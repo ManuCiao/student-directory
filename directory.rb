a838a78a6b9aeb2a50f54b0bcda4bca1fc9bdd0f
ManuCiao <mn.sabatino@gmail.com>
2016-09-16 01:16:20 +0200

    Add interactive menu to directory.rb

--- directory.rb
+++ directory.rb
@@ -75,7 +75,6 @@ end
 
 
 #8.8
-
 def group_by_cohort(students)
   
     #puts "#{students.group_by {|s, v| s[:Cohort]}.map{|s,v| puts "The #{s} Cohort has: "}}" + "\n" + "#{s[:Name]}, #{s[:Gender]}, #{s[:Nationality]}"   
@@ -83,7 +82,33 @@ def group_by_cohort(students)
     #puts "#{s[:Name]} - (#{s[:Cohort]} cohort), #{s[:Gender]}, #{s[:Nationality]}"      
       
 end
-  
+
+
+#9
+def interactive_menu
+  students = []
+  loop do
+    # 1. print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit" # 9 because we'll be adding more items
+    # 2. read the input and save it into a variable
+    selection = gets.chomp
+    # 3. do what the user has asked
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit # this will cause the program to terminate
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end 
 
 def print_footer(students)
   footer_title = "Overall, we have #{students.count} great students"
@@ -94,14 +119,14 @@ def print_footer(students)
   end
 end
 #nothing happens until we call the methods
-students = input_students
-print_header(students)
-print(students)
-
+#students = input_students
+#print_header(students)
+#print(students)
 #start_with(students)
 #short_name_12(students)
 #print_while(students)
-puts ''
-group_by_cohort(students)
-puts ''
-print_footer(students)
+#puts ''
+#group_by_cohort(students)
+#puts ''
+#print_footer(students)
+interactive_menu
