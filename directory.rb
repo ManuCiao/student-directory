2faf164a690b92f652497a1021a04c0224d12293
ManuCiao <mn.sabatino@gmail.com>
2016-09-15 04:01:58 +0200

    Complete from 8.5-8.10 exercises

--- directory.rb
+++ directory.rb
@@ -1,43 +1,44 @@
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  # create an empty array
+  puts "Please enter the details of each student. To finish, just hit return twice"
   students = []
-  # get the first name
-  name = gets.chomp
-
-  # while the name is not empty, repeat this code
-  while !name.empty? do  
-    puts "Which is the gender of the current Student? Enter male or female"
-    gender = gets.chomp
-    if gender != 'male' && gender != 'female' && gender == nil
-      puts "Gender must be 'male' or 'female', Try again.."
-      gender = gets.chomp
+  default = :September
+  loop do 
+    details = {Name: "Assign a value", Cohort: default, Gender: "Assign a value", Nationality: "Assign a value"}
+    details.each do |student, value|
+      puts "Assign a value to the #{student} of each student: "
+      value = gets.chomp.gsub(/\w+/, &:capitalize).to_sym
+      details[student] = value if !value.empty?          
     end
-      
-
     # add the student hash to the array
-    students << {name: name, cohort: :September, gender: gender, age}
-    puts "Now we have #{students.count} students"
-    puts "Please keep entering more names of the students or hit return twice to exit"
-    # get another name from the user
-    name = gets.chomp
+    students << details
+    note = "Now we have #{students.count} students"
+    if students.count <= 1
+      puts note.chop
+    else
+      puts note
+    end 
+    puts "Please enter more Students'details. Enter Y/N"
+    answer = gets.chomp.upcase
+    break if answer == "N" || answer.empty?
   end
   # return the array of students
   students
-
 end
 
-def print_header
-  puts "The students of my cohort at Makers Academy".center(50)
-  puts "-------------".center(50)
-  puts
+def print_header(students)
+  if students.count <= 1
+    puts "The student of my cohort at Makers Academy is ".center(100)
+  else
+    puts "The students of my cohort at Makers Academy are ".center(100)
+  end
+  puts "***************".center(100)
+  puts''
 end
 
 #8.1
 def print(students)
   students.each.with_index(1) do |student, index|
-    puts "#{index}. #{student[:name].capitalize!} - (#{student[:cohort]} cohort), #{student[:gender]}"      
+    puts "#{index}. #{student[:Name]} - (#{student[:Cohort]} cohort), #{student[:Gender]}, #{student[:Nationality]}".center(100)      
   end
 end
 
@@ -72,16 +73,35 @@ def print_while(students)
   end
 end
 
+
+#8.8
+
+def group_by_cohort(students)
+  
+    #puts "#{students.group_by {|s, v| s[:Cohort]}.map{|s,v| puts "The #{s} Cohort has: "}}" + "\n" + "#{s[:Name]}, #{s[:Gender]}, #{s[:Nationality]}"   
+    puts students.group_by {|s,v| s[:Cohort]}.map{|s,v| [s,v]}
+    #puts "#{s[:Name]} - (#{s[:Cohort]} cohort), #{s[:Gender]}, #{s[:Nationality]}"      
+      
+end
+  
+
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  footer_title = "Overall, we have #{students.count} great students"
+  if students.count <= 1
+    puts footer_title.chop.center(100)
+  else
+    puts footer_title.center(100)
+  end
 end
 #nothing happens until we call the methods
 students = input_students
-print_header
+print_header(students)
 print(students)
 
 #start_with(students)
 #short_name_12(students)
 #print_while(students)
-
+puts ''
+group_by_cohort(students)
+puts ''
 print_footer(students)
